0c474340b1a4590021ab27015599f2fe67d0c190
nemo-lssu <nemo@lssu.edu>
2026-05-13 13:00:27 -0400

    Update NEMO_ONXX.py

--- NEMO_ONXX.py
+++ NEMO_ONXX.py
@@ -1,13 +1,10 @@
 
 import os
-#os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 from ultralytics import YOLO
 
 # Build a YOLOv9c model from scratch
 model = YOLO("Feb_Model.pt")
 cwd = os.getcwd()
-# Train the model on the COCO8 example dataset for 100 epochs
-#results = model.train(data=f"{cwd}/BB_Augmented/data.yaml", epochs=50, imgsz=640,batch=8,device="cuda",patience = 25)
+
 model.export(format="onnx",dynamic=False)
 print ("[NEMO] Training complete. Success")
-#/media/amore/Vision_AGX1/NEMOTRAIN/YoloV9 Training Tutorial/AI_Training/RoboBoat2025_v3_Dataset
